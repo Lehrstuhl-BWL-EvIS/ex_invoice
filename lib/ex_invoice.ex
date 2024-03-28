477c19f1835d1958f7251f0eecc64ee90d20779b
krewast <bastian.kres@krewast.de>
2024-03-28 12:51:07 +0100

    Added files generated with "mix new ex_invoice"

---
+++ lib/ex_invoice.ex
@@ -0,0 +1,18 @@
+defmodule ExInvoice do
+  @moduledoc """
+  Documentation for `ExInvoice`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ExInvoice.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ex_invoice_test.exs
@@ -0,0 +1,8 @@
+defmodule ExInvoiceTest do
+  use ExUnit.Case
+  doctest ExInvoice
+
+  test "greets the world" do
+    assert ExInvoice.hello() == :world
+  end
+end
